7fe7d369ca1bf7df51046418cc7f23119baa249a
Yebin <39448821+Buyenix@users.noreply.github.com>
2018-08-04 15:03:04 +0800

    Excercise 1.5.2

---
+++ Chapter1-Fundamentals/CaseStudy/QuickUnionUF.java
@@ -0,0 +1,118 @@
+public class QuickUnionUF {
+    private int[] parent;  // parent[i] = parent of i
+    private int count;     // number of components
+    private int access;
+    /**
+     * Initializes an empty union¨Cfind data structure with {@code n} sites
+     * {@code 0} through {@code n-1}. Each site is initially in its own 
+     * component.
+     *
+     * @param  n the number of sites
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public QuickUnionUF(int n) {
+        parent = new int[n];
+        count = n;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between {@code 1} and {@code n})
+     */
+    public int count() {
+        return count;
+    }
+  
+    /**
+     * Returns the component identifier for the component containing site {@code p}.
+     *
+     * @param  p the integer representing one object
+     * @return the component identifier for the component containing site {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     */
+    public int find(int p) {
+        validate(p);
+        int old = access;
+        while (p != parent[p]) {
+            p = parent[p];
+            access += 2;
+        }
+        access++;
+        return p;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    /**
+     * Returns true if the the two sites are in the same component.
+     *
+     * @param  p the integer representing one site
+     * @param  q the integer representing the other site
+     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public boolean connected(int p, int q) {
+        access = 0;
+        boolean result = (find(p) == find(q));
+        StdOut.println("  connexted() access " + access);
+        return result;
+    }
+
+  
+    /**
+     * Merges the component containing site {@code p} with the 
+     * the component containing site {@code q}.
+     *
+     * @param  p the integer representing one site
+     * @param  q the integer representing the other site
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public void union(int p, int q) {
+        access = 0;
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP != rootQ) {
+            parent[rootP] = rootQ; 
+            count--;
+            access++;
+        }
+        StdOut.println("  union() access " + access);
+    }
+
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input, 
+     * where each integer represents some object;
+     * if the sites are in different components, merge the two components
+     * and print the pair to standard output.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        QuickUnionUF uf = new QuickUnionUF(10);
+        In in = new In("1_5_1.txt");
+        while (!in.isEmpty()) {
+            int p = in.readInt();
+            int q = in.readInt();
+            StdOut.printf("(%d, %d):\n", p, q);
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            //StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+
+}
