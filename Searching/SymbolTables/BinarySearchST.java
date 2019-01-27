86c6f822239e431b2d6cc432a5109e31dfb201df
Yebin <39448821+Buyenix@users.noreply.github.com>
2019-01-27 16:14:06 +0800

    3.1.16 ~ 3.1.18, 3.1.28, 3.1.30

--- Searching/SymbolTables/BinarySearchST.java
+++ Searching/SymbolTables/BinarySearchST.java
@@ -22,6 +22,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
     }
     public int rank(Key key)
     {
+        if (key == null) throw new IllegalArgumentException("argument to rank() is null");
         int lo = 0, hi = N-1;
         while(lo <= hi) {
             int mid = lo + (hi-lo)/2;
@@ -37,6 +38,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
     public boolean isEmpty() { return N == 0; }
     public Value get(Key key)
     {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
         if (isEmpty()) return null;
         int i = rank(key);
         if (i < N && keys[i].compareTo(key) == 0) return vals[i];
@@ -52,6 +54,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
     
     public boolean contains(Key key)
     {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         int i = rank(key);
         if (i < N && key.compareTo(keys[i])==0) return true;
         else                                    return false;
@@ -59,6 +62,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
     
     public void put(Key key, Value val)
     {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
         if (N == keys.length) resize(2*N);
         int cmp = -1;
         if (!isEmpty()) cmp = key.compareTo(keys[N-1]);
@@ -82,9 +86,11 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
             vals[i] = val;
             N++;
         }
+        assert check();
     }
     
     public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
         int i = rank(key);
         if (i < N && key.compareTo(keys[i]) == 0) {
             for (int j = i; j < N-1; j++) {
@@ -94,32 +100,39 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
             N--;
             if (N > 0 && N == keys.length/4) resize(keys.length/2);
         }
-        return;
+        assert check();
     }
     
     public Key min()
     {
+        if (isEmpty()) throw new IllegalArgumentException("called min() with empty symbol table");
         return keys[0];
     }
     
     public Key max()
     {
+        if (isEmpty()) throw new IllegalArgumentException("called max() with empty symbol table");
         return keys[N-1];
     }
     
     public Key select(int k)
     {
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException("called select() with invalid argument: " + k);
+        }
         return keys[k];
     }
     
     public Key ceiling(Key key)
     {
+        if (key == null) throw new IllegalArgumentException("argument to ceiling() is null");
         int i = rank(key);
         return keys[i];
     }
     
     public Key floor(Key key)
     {
+        if (key == null) throw new IllegalArgumentException("argument to floor() is null");
         int i = rank(key);
         if (i >= N) return null;
         if (key.compareTo(keys[i]) == 0) {
@@ -133,6 +146,8 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
     
     public Iterable<Key> keys(Key lo, Key hi)
     {
+        if (lo == null) throw new IllegalArgumentException("first argument to keys() is null"); 
+        if (hi == null) throw new IllegalArgumentException("second argument to keys() is null"); 
         Queue<Key> q = new Queue<Key>();
         int lo_idx = rank(lo);
         int hi_idx = rank(hi);
@@ -152,6 +167,26 @@ public class BinarySearchST<Key extends Comparable<Key>, Value>
             q.enqueue(keys[i]);
         }
         return q;
+    }
+    
+        private boolean check() {
+        return isSorted() && rankCheck();
+    }
+
+    // are the items in the array in ascending order?
+    private boolean isSorted() {
+        for (int i = 1; i < size(); i++)
+            if (keys[i].compareTo(keys[i-1]) < 0) return false;
+        return true;
+    }
+
+    // check that rank(select(i)) = i
+    private boolean rankCheck() {
+        for (int i = 0; i < size(); i++)
+            if (i != rank(select(i))) return false;
+        for (int i = 0; i < size(); i++)
+            if (keys[i].compareTo(select(rank(keys[i]))) != 0) return false;
+        return true;
     }
 }
 
