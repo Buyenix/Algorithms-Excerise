6d8b9dbe8c765f29b7806177036205ed977a591f
Yebin <39448821+Buyenix@users.noreply.github.com>
2019-02-23 18:10:51 +0800

    3.2.5 ~ 3.2.7

--- Searching/BinarySearchTrees/BST.java
+++ Searching/BinarySearchTrees/BST.java
@@ -25,6 +25,7 @@ public class BST<Key extends Comparable<Key>, Value>
         else           return x.N;
     }
     
+    public boolean isEmpty() { return size() == 0; }
     private int height(Node x)
     {
        if (x == null) return 0;
@@ -41,6 +42,8 @@ public class BST<Key extends Comparable<Key>, Value>
         else               return get(x.right, key);
     }
     
+    public boolean contains(Key key) { return get(key) != null; }
+    
     public void put(Key key, Value val) { root = put(root, key, val); }
     private Node put(Node x, Key key, Value val)
     {
@@ -56,30 +59,24 @@ public class BST<Key extends Comparable<Key>, Value>
     
     public Key min()
     {
-        if (root == null) return null;
-        Node min = min(root);
-        if (min == null) return null;
-        else             return min.key;
+        if (isEmpty()) throw new RuntimeException("calls min() with empty symbol table");
+        return min(root).key;
     }
     private Node min(Node x)
     {
-        if (x == null)           return null;
-        else if (x.left == null) return x;
-        else                     return min(x.left);
+        if (x.left == null) return x;
+        else                return min(x.left);
     }
     
     public Key max()
     {
         if (root == null) return null;
-        Node max = max(root);
-        if (max == null) return null;
-        else             return max.key;
+        return max(root).key;
     }
     private Node max(Node x)
     {
-        if (x == null)           return null;
-        else if (x.right == null) return x;
-        else                     return max(x.right);
+        if (x.right == null) return x;
+        else                 return max(x.right);
     }
     
     public Key floor(Key key)
@@ -99,6 +96,18 @@ public class BST<Key extends Comparable<Key>, Value>
         else           return t;
     }
  
+    public Key floor2(Key key) {
+        return floor2(root, key, null);
+    }
+
+    private Key floor2(Node x, Key key, Key best) {
+        if (x == null) return best;
+        int cmp = key.compareTo(x.key);
+        if      (cmp  < 0) return floor2(x.left, key, best);
+        else if (cmp  > 0) return floor2(x.right, key, x.key);
+        else               return x.key;
+    }
+    
     public Key ceiling(Key key)
     {
         Node x = ceiling(root, key);
@@ -139,10 +148,19 @@ public class BST<Key extends Comparable<Key>, Value>
         else return size(x.left) + 1 + rank(x.right, key);
     }
     
+    public int size(Key lo, Key hi)
+    {
+        if (lo.compareTo(hi) > 0) return 0;
+        int rankL = rank(lo);
+        int rankH = rank(hi);
+        if (contains(hi)) return rankH - rankL + 1;
+        else              return rankH - rankL;
+    }
+    
     public void deleteMin() { root = deleteMin(root); }
     private Node deleteMin(Node x)
     {
-        if (x == null) return null;
+        //if (x == null) return null;
         if (x.left == null) return x.right;
         x.left = deleteMin(x.left);
         x.N = size(x.left) + size(x.right) + 1;
@@ -161,17 +179,19 @@ public class BST<Key extends Comparable<Key>, Value>
             if (x.left == null) return x.right;
             if (x.right == null) return x.left;
             Node t = x;
-            x = min(x.right);
+            x = min(t.right);
             x.left = t.left;
             x.right = deleteMin(t.right);
         }
         x.N = size(x.left) + size(x.right) + 1;
+        //StdOut.println("size left = " + size(x.left));
+        //StdOut.println("size right = " + size(x.right));
         x.H = Math.max(height(x.left), height(x.right)) + 1;
         return x;
     }
     
     public Iterable<Key> keys() { return keys(min(), max()); }
-    private Iterable<Key> keys(Key lo, Key hi)
+    public Iterable<Key> keys(Key lo, Key hi)
     {
         Queue<Key> q = new Queue<Key>();
         keys(root, q, lo, hi);
@@ -181,8 +201,8 @@ public class BST<Key extends Comparable<Key>, Value>
     private void keys(Node x, Queue<Key> q, Key lo, Key hi)
     {
         if (x == null) return;
-        int loCmp = x.key.compareTo(lo);
-        int hiCmp = x.key.compareTo(hi);
+        int loCmp = lo.compareTo(x.key);
+        int hiCmp = hi.compareTo(x.key);
         if (loCmp < 0) keys(x.left, q, lo, hi);
         if (loCmp <= 0 && hiCmp >= 0) q.enqueue(x.key);
         if (hiCmp > 0) keys(x.right, q, lo, hi);
@@ -192,10 +212,24 @@ public class BST<Key extends Comparable<Key>, Value>
     public int height2() { return height2(root); }
     private int height2(Node x)
     {
-        if (x == null) return 0;
+        if (x == null) return -1;
         int lHeight = height(x.left);
         int rHeight = height(x.right);
         return 1 + Math.max(lHeight, rHeight);
+    }
+    
+    public double avgCompares() {
+        int size = size();
+        if (size == 0) return 0;
+        else {
+            int internalPath = internalPath(root);
+            return (double)(internalPath/size) + 1;
+        }
+    }
+    private int internalPath(Node x)
+    {
+        if (x == null) return -1;
+        else return 2 + internalPath(x.left) + internalPath(x.right);
     }
 }
 
